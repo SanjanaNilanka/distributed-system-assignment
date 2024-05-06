8bdb2ef4ef3464bb94ea9b6050dafd22dd8a7a13
Hasindu Ranasinghe <hasinduranasingheb2098@gmail.com>
2024-05-06 20:29:03 +0530

    Payment Service Database Connection Initialization

--- payment-service/paymentServiceServer.js
+++ payment-service/paymentServiceServer.js
@@ -10,6 +10,14 @@ app.use(bodyParse.json());
 
 
 const PORT = 5000;
+const DB_URL = "mongodb+srv://hasinduranasingheb2098:o2ndrPgUPHjk6KlF@cluster0.fhhrtiv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(DB_URL).then(() => {
+    console.log("Payment Service Database Connected Successfully");
+}).catch((error) => {
+    console.log("Payment Service Database Connection Unsuccessful");
+    console.log(error);
+});
 
 app.listen(PORT, () => {
     console.log(`Payment Service Server is running on PORT ${PORT}`);
